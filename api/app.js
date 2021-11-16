9fb50831ec1cbbfcd95c34a87c2be579ead255ef
Sampo Vuorento <t0vusa01@students.oamk.fi>
2021-11-16 15:15:26 +0200

    restapi toimii kortti tili asiakas

--- api/app.js
+++ api/app.js
@@ -2,9 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var asiakasRouter = require('./routes/asiakas');
+var tiliRouter = require('./routes/tili');
+var korttiRouter = require('./routes/kortti');
 
 var app = express();
 
@@ -16,5 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/asiakas', asiakasRouter);
+app.use('/tili', tiliRouter);
+app.use('/kortti', korttiRouter);
 
 module.exports = app;
