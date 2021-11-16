172a1a8fff580ca209620d784faf7ee06b914068
Sampo Vuorento <t0vusa01@students.oamk.fi>
2021-11-16 19:35:34 +0200

    lisätty salaus pin-koodiin

--- api/models/kortti_model.js
+++ api/models/kortti_model.js
@@ -1,22 +1,30 @@
 const db = require('../database');
 const bcrypt = require('bcryptjs');
 
-const saltRounds = 10;
+const saltRounds=10;
 const kortti={
-    get: function(callback) {
-        return db.query('select * from kortti', callback);
-    },
-    getById: function(id, callback) {
-        return db.query('select * from kortti where idKortti=?', [id], callback);
-    },
-    add: function(kortti, callback) {
-        return db.query('insert into kortti (pin, idAsiakas) values (?, ?)', [kortti.pin, kortti.idAsiakas], callback);
-    },
-    delete: function(id, callback) {
-        return db.query('delete from kortti where idKortti=?', [id], callback);
-    },
-    update: function(id, kortti, callback) {
-        return db.query('update kortti set pin=?, idAsiakas=? where idKortti=?', [kortti.pin, kortti.idAsiakas, id], callback);
-    }
+  get: function(callback) {
+    return db.query('select * from kortti', callback);
+  },
+  getById: function(id, callback) {
+    return db.query('select * from kortti where idKortti=?', [id], callback);
+  },
+  add: function(kortti, callback) {
+    bcrypt.hash(kortti.pin, saltRounds, function(err, hash) {
+      return db.query('insert into kortti (idKortti, pin, idAsiakas) values (?,?,?)',
+      [kortti.idKortti, hash, kortti.idAsiakas], callback);
+    });
+  },
+  delete: function(id, callback) {
+    return db.query('delete from kortti where idKortti=?', [id], callback);
+  },
+  update: function(id, kortti, callback) {
+    bcrypt.hash(kortti.pin, saltRounds, function(err, hash) {
+      return db.query('update kortti set idKortti=?, pin=? where idTili=?',
+      [kortti.idKortti, hash, id], callback);
+    });
+  }
+
 }
+
 module.exports = kortti;
