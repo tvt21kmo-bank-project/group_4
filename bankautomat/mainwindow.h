e4db4ab5cfebeaf37a4d044f489d529c3702ec90
Sampo Vuorento <t0vusa01@students.oamk.fi>
2021-11-17 19:28:49 +0200

    muokattu QT toimimaan meidän login tiedoilla

--- bankautomat/mainwindow.h
+++ bankautomat/mainwindow.h
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "pankki.h"
+
 #include <QMainWindow>
+#include <QtNetwork>
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +20,22 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btnShowBooks_clicked();
+    void getBookSlot (QNetworkReply *reply);
+    void getOneBookSlot (QNetworkReply *reply);
+    void loginSlot (QNetworkReply *reply);
+
+    void on_btnShowOneBook_clicked();
+
+    void on_btnLogin_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QNetworkAccessManager *manager;
+    QNetworkAccessManager *oneBookManager;
+    QNetworkAccessManager *loginManager;
+    QNetworkReply *reply;
+    Pankki *objPankki;
 };
 #endif // MAINWINDOW_H
