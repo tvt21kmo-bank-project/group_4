2a6a8f67c2e0268a084ae0c14b21a1cc502714a1
Massajah <osmo666@gmail.com>
2021-12-11 11:03:01 +0200

    takaisin painikkeet ja pientä säätöä

--- bankautomat/nosto.cpp
+++ bankautomat/nosto.cpp
@@ -17,32 +17,32 @@ Nosto::~Nosto()
 
 void Nosto::on_btn20_clicked()
 {
-
+    ui->leSsumma->setText("20");
 }
 
 void Nosto::on_btn40_clicked()
 {
-
+    ui->leSsumma->setText("40");
 }
 
 void Nosto::on_btn60_clicked()
 {
-
+    ui->leSsumma->setText("60");
 }
 
 void Nosto::on_btn100_clicked()
 {
-
+    ui->leSsumma->setText("100");
 }
 
 void Nosto::on_btn200_clicked()
 {
-
+    ui->leSsumma->setText("200");
 }
 
 void Nosto::on_btn500_clicked()
 {
-
+    ui->leSsumma->setText("500");
 }
 
 void Nosto::on_btnHyvaksy_clicked()
@@ -53,5 +53,5 @@ void Nosto::on_btnHyvaksy_clicked()
 void Nosto::on_btnTakaisin_clicked()
 {
     this -> close();
-    //objValikko -> show();
+
 }
